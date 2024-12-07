b0098c988ecf870aeb8dfbe78bc38b28ec7c89ec
xhc12345 <hcxu@utexas.edu>
2024-12-07 04:12:18 -0600

    minor main changes

--- src/main.py
+++ src/main.py
@@ -5,22 +5,27 @@ from prompts.second_detect import flaky_detect_2
 from prompts.test_apis import test_api
 
 DATA_PATH = os.getenv("DATA_PATH", "./data")
-TEST1 = f"{DATA_PATH}/test-suites/example/FlakyTestSuite.java"
-TEST2 = f"{DATA_PATH}/test-suites/example/FlakyTestSuiteObfuscated.java"
+EXAMPLE_1 = f"{DATA_PATH}/test-suites/example/FlakyTestSuite.java"
+EXAMPLE_2 = f"{DATA_PATH}/test-suites/example/FlakyTestSuiteObfuscated.java"
 
-
-def main():
-    # test_api()
-
-    test_path = TEST1
+def process_test(test_path):
     # Load your Java file content
     with open(test_path, "r") as file:
         test_code = file.read()
 
     flaky_detect_1(test_path, test_code)
-
     res_gpt, res_gemini, res_llama = read_all_from_output()
+
     flaky_detect_2(result_gpt=res_gpt, result_gemini=res_gemini, result_llama=res_llama)
+    res_gpt, res_gemini, res_llama = read_all_from_output()
+    
+
+def test_example():
+    process_test(EXAMPLE_1)
+
+def main():
+    # test_api()
+    test_example()
 
 
 if __name__ == "__main__":
